5b41dcd761d42f2d8406035a16f4aa7bb146272d
sametkarademir <sametkarademir244@gmail.com>
2026-01-14 21:52:52 +0300

    fix: add react-is dependency and update vite config for recharts build

--- taskflow-react-client/vite.config.ts
+++ taskflow-react-client/vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["react-is"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-is/, /node_modules/],
+    },
+  },
 });
